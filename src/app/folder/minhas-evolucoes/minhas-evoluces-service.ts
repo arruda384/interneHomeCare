904b7cb4d51bae9f40890b5f6abf3c133636f6bd
Rodrigo Arruda de Araújo <rodrigo.araujo@interne.com.br>
2021-06-06 10:40:00 -0300

    teste pwa

--- src/app/folder/minhas-evolucoes/minhas-evoluces-service.ts
+++ src/app/folder/minhas-evolucoes/minhas-evoluces-service.ts
@@ -3,13 +3,20 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Configuracao } from 'src/app/util/Configuracao';
 import { UtilProvider } from 'src/app/util/util';
 import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { ListaEvolucoes } from './lista-evolucoes';
+import { map } from 'rxjs/operators';
 
-
+interface ListaEvolucoesResponse{
+    Evolucoes: Array<any>;
+}
 @Injectable()
 export class EvolucaoService{
   urlApi = environment.apiUrl;
   mvPepApi = environment.mvApiPepHomeCare;
   mvAnexoApi = environment.mvApiPepHomeCare;
+
+  private urlPep = 'http://168.138.224.173:8042/mv-api-pep-home-care/solicitacao/listarEvolucoes/?codigoUsuario=TI.MEDICO&dataInicio=01%2F05%2F2021&dataFim=01%2F06%2F2021&codigoPaciente=0';
 
     confSinaisVitais: Configuracao = new Configuracao();
 
@@ -37,9 +44,6 @@ export class EvolucaoService{
 
     public retornarListaEvolucoes(codigoUsuario: string, dataInicio: string, dataFim: string, codigoPaciente: string) {
         // let url = 'localhost:8080' + '/mv-api-pep-home-care/solicitacao/listarEvolucoes/';
-
-      
-
         let url = this.urlApi + this.mvPepApi+ '/solicitacao/listarEvolucoes/';
         url = url + '?codigoUsuario=' + codigoUsuario;
         url = url + '&dataInicio=' + dataInicio;
@@ -50,6 +54,13 @@ export class EvolucaoService{
         return this.http
             .get<any>(url, { params: params})
     }
+
+    // getPEP(): Observable<ListaEvolucoes[]>{
+    //     return this.http.jsonp(this.urlPep, 'callback')
+    //     .pipe(
+    //         map((res: ListaEvolucoesResponse) => this.lissta(res.Evolucoes))
+    //     )
+    // }
 
     public cancelarEvolucao(evolucao){   
         let url = this.urlApi + this.mvPepApi+ '/cancelarEvolucao/';
