1555f2e6aca06467719332b69ddc1f083b0a3f1b
Rodrigo Arruda de Araújo <rodrigo.araujo@interne.com.br>
2021-06-06 11:11:38 -0300

    ajuste da  url api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
-  mvApiDocumentoAnexo: "/mv-api-documento-anexo-prod",
-  mvApiPepHomeCare: "/mv-api-pep-home-care-prod",
+  apiUrl: 'http://168.138.224.173:8042',
+  mvApiDocumentoAnexo: "mv-api-documento-anexo",
+  mvApiPepHomeCare: "mv-api-pep-home-care",
 };
